d4c33b54ce4eca0e6ee2d75ba30cfe278bc4be13
Angel Munoz <amunoz@nearsoft.com>
2014-08-28 23:30:26 -0700

    Testing environment setup

---
+++ test/test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by angel on 28/08/14.
+ */
+describe('suite test', function(){
+    it('should add two numbers', function(){
+        expect(4).to.equal(4);
+    });
+
+    it('should subtract two numbers', function(){
+        expect(4-2).to.equal(2);
+    });
+});
